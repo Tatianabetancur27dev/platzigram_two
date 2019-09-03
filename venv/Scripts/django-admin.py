149cb8d0e4c96ccbce262ff0be59429a761b592d
Tatiana <Tatianabetancur27dev@gmail.com>
2019-09-03 09:24:09 -0500

    Init project platzigram_two !!!

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tbetancur\documents\c-01\platzigram_two\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
