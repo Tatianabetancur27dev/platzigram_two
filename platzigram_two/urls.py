1c4db069dfc9194591326e103c6b0881cebf8fe7
Tatiana <Tatianabetancur27dev@gmail.com>
2019-09-03 16:48:22 -0500

    Requirements

--- platzigram_two/urls.py
+++ platzigram_two/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
+from django.conf.urls.static import static
+
 from users import views as views_users
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('users/', views_users.index),
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
