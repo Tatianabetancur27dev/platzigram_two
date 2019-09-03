11d6be36a1e9d6e0fb4874736da066c8af89b569
Tatiana <Tatianabetancur27dev@gmail.com>
2019-09-03 14:27:16 -0500

    Actions admin module

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,19 @@ from users.models import Profile
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    pass
+    
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('pk', 'user')
+    list_editable = ('phone_number', 'website', 'picture')
+    search_fields = (
+        'user__email', 
+        'user__first_name', 
+        'user__last_name', 
+        'phone_number'
+        )
+    list_filter = (
+        'create',
+        'modified',
+        'user__is_active',
+        'user__is_staff',
+    )
