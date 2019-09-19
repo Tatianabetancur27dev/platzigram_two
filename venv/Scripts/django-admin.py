efe86e26df7fe895df7a6a18ca37568cb09911e9
Tatiana Betancur <tatianabetancur27dev@hotmail.com>
2019-09-18 23:04:50 -0500

    Git ignore, delete archives

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\tbetancur\documents\c-01\platzigram_two\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
